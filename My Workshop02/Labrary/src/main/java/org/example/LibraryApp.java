98b8c03ae0a3591a7096fe411179f3f7cb92718e
JNL001O <166418057+JNL001O@users.noreply.github.com>
2024-04-22 11:03:45 -0500

    Update LibraryApp.java

--- My Workshop02/Labrary/src/main/java/org/example/LibraryApp.java
+++ My Workshop02/Labrary/src/main/java/org/example/LibraryApp.java
@@ -11,8 +11,27 @@ public class LibraryApp {
     }
     private static void intializeInventory(){
         //Adding 20 sample books to the inventory
-        for (int i = 1; i <= 20; i++){
-            inventory.add(new Book(i, "ISBN" + i, "Book" + i));
+       // Adding 20 books to the inventory
+        inventory.add(new Book(1, "9780141439600", "Pride and Prejudice"));
+        inventory.add(new Book(2, "9780061120084", "To Kill a Mockingbird"));
+        inventory.add(new Book(3, "9780451524935", "1984"));
+        inventory.add(new Book(4, "9780743273565", "The Great Gatsby"));
+        inventory.add(new Book(5, "9781451673319", "The Catcher in the Rye"));
+        inventory.add(new Book(6, "9780060850524", "The Hobbit"));
+        inventory.add(new Book(7, "9780143111597", "Moby-Dick"));
+        inventory.add(new Book(8, "9780141439792", "Jane Eyre"));
+        inventory.add(new Book(9, "9780553212471", "War and Peace"));
+        inventory.add(new Book(10, "9780449212606", "Crime and Punishment"));
+        inventory.add(new Book(11, "9780486415871", "Alice's Adventures in Wonderland"));
+        inventory.add(new Book(12, "9780141439471", "Wuthering Heights"));
+        inventory.add(new Book(13, "9781593080279", "Anna Karenina"));
+        inventory.add(new Book(14, "9780486270523", "Dracula"));
+        inventory.add(new Book(15, "9780486282113", "Frankenstein"));
+        inventory.add(new Book(16, "9780141439563", "Sense and Sensibility"));
+        inventory.add(new Book(17, "9780141192462", "The Picture of Dorian Gray"));
+        inventory.add(new Book(18, "9780486284735", "Les Misérables"));
+        inventory.add(new Book(19, "9780452284247", "Brave New World"));
+        inventory.add(new Book(20, "9780486280614", "The Scarlet Letter"));
         }
     }
     private static void showHomeScreen(){
